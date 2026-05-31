f41494b67c5a1a38e02b27e74c52d72c1585ccfa
Darkness <152873138+Itz-Npg@users.noreply.github.com>
2026-05-31 11:56:13 +0530

    Update zenhubbf.lua

--- zenhubbf.lua
+++ zenhubbf.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://vss.pandadevelopment.net/virtual/file/c2fb4114c409436d"))()
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/e2a14c5549a0ba62085c4cc3726ede1d57a3d0d48f1f2a7ae7c662f0afd05de8/download"))()
